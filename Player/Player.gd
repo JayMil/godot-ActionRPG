34cfb8505a4b0f44032ecd3230574fa02c3879b7
JayMil <jmilder.lp@gmail.com>
2022-01-26 16:13:50 -0600

    Add collision for player and random area

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,21 +1,21 @@
 extends KinematicBody2D
 
-const ACCELERATION = 15
+const ACCELERATION = 25
 const MAX_SPEED = 100
 const FRICTION = 15
 
 var velocity = Vector2.ZERO
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION)
 		
-	move_and_collide(velocity)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+		
+	velocity = move_and_slide(velocity)
