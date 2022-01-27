dd7e1e5541cab52b16f767c724fe90e7cd5d3230
JayMil <jmilder.lp@gmail.com>
2022-01-27 13:25:03 -0600

    P16: Add basic bat AI to chase player

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,18 +2,62 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
-var knockback = Vector2.ZERO
+export var ACCELERATION = 4
+export var MAX_SPEED = 100
+export var FRICTION = 5
+export var KNOCKBACK = 200
 
 onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
+onready var sprite = $AnimatedBatSprite
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var velocity = Vector2.ZERO
+var knockback = Vector2.ZERO
+
+var state = IDLE
+
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:   idle_state(delta)
+		WANDER: wander_state()
+		CHASE:  chase_state(delta)
+	
+	velocity = move_and_slide(velocity)
 
 
+func idle_state(delta):
+	velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+	seek_player()
+
+func wander_state():
+	pass
+	
+func chase_state(delta):
+	var player = playerDetectionZone.player
+	if player != null:
+		var direction = (player.global_position - global_position).normalized()
+		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION)
+	else:
+		state = IDLE
+	sprite.flip_h = velocity.x < 0
+	
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
+	
 func _on_Hurtbox_area_entered(hitbox):
 	stats.damage(hitbox.damage)
-	knockback = hitbox.knockback_vector * 120
+	knockback = hitbox.knockback_vector * KNOCKBACK
 	
 
 
