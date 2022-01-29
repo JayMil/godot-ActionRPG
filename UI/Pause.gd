4466fc5ac3173031831d09df4f75f38ae6985d81
JayMil <jmilder.lp@gmail.com>
2022-01-29 11:48:22 -0600

    Add quit button to pause menu

--- UI/Pause.gd
+++ UI/Pause.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 onready var overlay = $Pause
+onready var quitButton = $Pause/VBoxContainer/QuitButton
 
 func _ready():
 	overlay.visible = false
@@ -10,3 +11,8 @@ func _input(event):
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = new_pause_state
 		overlay.visible = new_pause_state
+		quitButton.grab_focus()
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
